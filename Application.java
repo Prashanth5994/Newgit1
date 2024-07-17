151492fcab707e3560d8662fc5637932c9187f85
Prashanth5994 <prasanth.ch1604@gmail.com>
2024-07-17 19:52:52 +0530

    feature201 changes in Application.java file

--- Application.java
+++ Application.java
@@ -15,5 +15,23 @@ public class HelloWorld {
         // println() prints the following line to the output screen
         System.out.println("You entered: " + number);
     }
+#feature201 changes by prashu on payment gateway module oct2023
+import java.util.Scanner;
 
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
+    }
 }
+feature201 changes end
