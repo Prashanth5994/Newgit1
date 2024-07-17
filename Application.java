e26b414ba7048feaaccd398afb203fdfb4832063
Prashanth5994 <prasanth.ch1604@gmail.com>
2024-07-17 20:02:24 +0530

    feature202 changes

--- Application.java
+++ Application.java
@@ -15,5 +15,17 @@ public class HelloWorld {
         // println() prints the following line to the output screen
         System.out.println("You entered: " + number);
     }
+#feature202 by sai for reports module in oct2023
+public class MultiplyTwoNumbers {
 
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+#feature202 end
 }
